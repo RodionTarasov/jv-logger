075ee0159b5299194cdc438af7c77fe0760386b0
kozma <kozmakruckov@gmail.com>
2025-08-19 22:05:36 +0300

    added jv-logger solution 1

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -2,21 +2,27 @@ package mate.academy.service;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
+        logger.info("login method was called. Params: Login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
-            throw new AuthenticationException("Username or password are incorrect");
+            logger.error("login failed. Username: {}", login);
+            throw new AuthenticationException("username or password are incorrect");
         }
+        logger.info("user {} logged in successfully", login);
         return user;
     }
 
     private User findByLogin(String login) {
+        logger.info("find by login method was called. Params: Login = {}", login);
         User user = new User(login, "1234");
-        // this user identifier should be set by DB. We will use dummy data for this example
         user.setUserId(2L);
         return user;
     }
